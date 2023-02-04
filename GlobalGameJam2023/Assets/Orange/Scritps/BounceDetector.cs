9b41d41e8f9dc7999973207e1420bd974d2756b5
XinyuanD <xinyuand6@gmail.com>
2023-02-03 21:26:54 -0800

    Added EnemyBehavior script
    Enemy can patrol between two way points, and the enemy can get stunned if the player bounces on its head

---
+++ GlobalGameJam2023/Assets/Orange/Scritps/BounceDetector.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceDetector : MonoBehaviour
+{
+    public bool gotBounced = false;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gotBounced = true;
+        }
+    }
+
+}
