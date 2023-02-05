662fe60e6c075eb00d5c38b5f451531fa4909d1c
Grimmethy <choffco@hotmail.com>
2023-02-05 09:50:51 -0700

    Animations and controller work

--- GlobalGameJam2023/Assets/Orange/Scritps/PlayerMovement.cs
+++ GlobalGameJam2023/Assets/Orange/Scritps/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -22,17 +23,36 @@ public class PlayerMovement : MonoBehaviour
     public float coyoteTimeCounter;
     public float spriteScale;
 
+    public int wallJumpCount = 1;
+    private int wallJumps;
+
     [Header("Movements Due to Environment")]
     public bool onGround = false;
     public bool mushroomJump = false;
     public float mushroonJumpSpeed = 15f;
     public float enemyBounceSpeed = 15f;
 
+    private Vector3 lastPos;
+
+    private Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+    }
+
     void Start()
     {
         bodyCollider = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         spriteScale = transform.localScale.x;
+
+        lastPos = transform.position;
+    }
+
+    void Update()
+    {
+        SetRunAnim();
     }
 
     void FixedUpdate()
@@ -48,15 +68,21 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
+        // coyote timer is for allowing the player to jump if they have just left the ground without jumping
+        // I'm not a fan of this jump mechanic, it feels clumsy. I'd like to find a way to reintegrate different jump heights using addforce for a smoother experience
         if (coyoteTimeCounter > 0f && context.started)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+            anim.SetBool("Jumping", true);
+            anim.SetBool("Falling", false);
         }
 
         if (context.canceled && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * jumpHeightModifier);
             coyoteTimeCounter = 0f;
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Falling", true);
         }
         
     }
@@ -65,9 +91,10 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
 
-        if (IsOnVine())
+        if (IsOnVine() && anim.GetBool("Jumping") == false)
         {
             rb.velocity = new Vector2(rb.velocity.x, moveInput.y * moveSpeed);
+            coyoteTimeCounter = coyoteTime;
         }
 
         if (moveInput.x > 0)
@@ -85,7 +112,10 @@ public class PlayerMovement : MonoBehaviour
         onGround = IsGrounded();
         if (onGround)
         {
+            wallJumps = 0;
             coyoteTimeCounter = coyoteTime;
+            anim.SetBool("Jumping", false);
+            anim.SetBool("Falling", false);
         }
         else
         {
@@ -115,6 +145,13 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
+        if (other.gameObject.tag == "Ground" && wallJumps < wallJumpCount)
+        {
+            onGround = true;
+            coyoteTimeCounter = coyoteTime;
+            wallJumps++;
+        }
+
         // Check if is enemy
         if (other.gameObject.tag == "Enemy")
         {
@@ -132,5 +169,19 @@ public class PlayerMovement : MonoBehaviour
         {
             gameManager.InvokePlayerDeath();
         }
+    }
+
+    void SetRunAnim()
+    {
+        Vector3 posChange = transform.position - lastPos;
+        if (Mathf.Abs(posChange.x) >= .01f || Mathf.Abs(posChange.z) >= .01f)
+        {
+            anim.SetBool("Moving", true);
+        }
+        else
+        {
+            anim.SetBool("Moving", false);
+        }
+        lastPos = transform.position;
     }
 }
